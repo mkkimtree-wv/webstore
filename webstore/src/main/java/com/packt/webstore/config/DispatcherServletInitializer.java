2ec45c03089d78b525af0eb313acf76c7d81b9bc
mk_kim <mk_kim@warevalley.com>
2018-01-04 14:48:14 +0900

    servlet mapping change / -> /app/*

--- webstore/src/main/java/com/packt/webstore/config/DispatcherServletInitializer.java
+++ webstore/src/main/java/com/packt/webstore/config/DispatcherServletInitializer.java
@@ -15,6 +15,6 @@ public class DispatcherServletInitializer extends AbstractAnnotationConfigDispat
 
 	@Override
 	protected String[] getServletMappings() {
-		return new String[] { "/" };
+		return new String[] { "/app/*" };
 	}
 }
